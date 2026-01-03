bb9229395742b19d3ff63aff09435e248f465747
Khaled Elhanan <deathnotecode1@gmail.com>
2026-01-03 04:17:45 +0200

    remove temp test file

--- test_build.ps1
+++
@@ -1,2 +0,0 @@
-$output = dotnet build "c:\Users\khald\source\repos\Tenantix Platform\Tenantix.Infrastructure\Tenantix.Infrastructure.csproj" 2>&1
-$output | Out-File "c:\Users\khald\source\repos\Tenantix Platform\build_output_full.txt"
